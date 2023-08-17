85a7a1e41a9f82dd8b61b10c8f20fbfa5fab5406
Trevor Tress <trevor.tress@outlook.com>
2023-08-17 13:42:21 -0400

    home refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,12 @@
 'use client';
-import Carousel from '@/components/Carousel';
-import CycleHeading from '@/components/CycleHeading';
-import { FC } from 'react';
+import LandingPage from '@/components/Home/Home';
+import Dashboard from '@/components/Home/Dashboard';
+import { FC, useState } from 'react';
 import styled from 'styled-components';
 
-const Dashboard: FC<{ className?: string }> = ({ className }) => {
-	return (
-		<>
-			<div className={className}>
-				<div className="xxx">
-					<CycleHeading />
-					<p>this app is literally so flippin cool dude. like for real. just try it bro. im serious. you wont regret it bro. plz try our app bro. you need to. like right now bro.</p>
-				</div>
-				<div className="yyy">
-					<label>Email</label>
-					<input
-						type="text"
-						placeholder="Enter username"
-					></input>
-					<label>Password</label>
-					<input
-						type="text"
-						placeholder="Enter password"
-					></input>
-				</div>
-			</div>
-
-			<Carousel />
-		</>
-	);
+const Home: FC<{ className?: string }> = ({ className }) => {
+	const [loggedIn, setLoggedIn] = useState<boolean>(false);
+	return !loggedIn ? <LandingPage /> : <Dashboard />;
 };
 
-export default styled(Dashboard)`
-	display: flex;
-
-	& .xxx,
-	.yyy {
-		background: #c0c0c0b3;
-		text-align: center;
-		font-weight: 700;
-		border-radius: 2rem;
-		margin: 3rem 2rem;
-		box-shadow: 2px 2px 12px -1px rgba(0, 0, 0, 0.75);
-	}
-
-	& .xxx {
-		width: 65vw;
-		& p {
-			font-size: 30px;
-		}
-	}
-
-	& .yyy {
-		width: 24vw;
-
-		& input {
-			display: block;
-			margin: auto;
-			width: 70%;
-			border-radius: 1rem;
-			padding: 1rem;
-		}
-
-		& label {
-			font-size: 2rem;
-		}
-	}
-`;
+export default styled(Home)``;
---
+++ components/Home/Dashboard.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import Carousel from '@/components/Carousel';
+import CycleHeading from '@/components/CycleHeading';
+import { FC } from 'react';
+
+const Home: FC<{ className?: string }> = ({ className }) => {
+	return <></>;
+};
+
+export default styled(Home)``;
